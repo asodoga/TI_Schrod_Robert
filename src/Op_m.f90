5cd5d5cfa524486ae5a3333012ca565f240c6b0f
AFANSOUNOUDJI <afankokourobert@gmail.com>
2022-02-15 18:56:25 +0000

    La version2 15_02_2022

--- src/Op_m.f90
+++ src/Op_m.f90
@@ -84,7 +84,7 @@ contains
         V(iq)=Calc_pot(Q)  !Calc_pot(Q(1))+ Calc_pot(Q(2))
 
         !V(iq) = Calc_pot(Basis%tab_basis(1)%x(iq1))+Calc_pot(Basis%tab_basis(2)%x(iq2))
-        Write(10,*) "iq1,iq2,iq,V(iq)" ,iq1,iq2,iq,Q(:),V(iq)
+        Write(10,*) Q(:),V(iq)
       END DO
       END DO
 
@@ -99,18 +99,21 @@ contains
          DO iq2=1,Basis%tab_basis(2)%nq
            iq=iq+1
            OpPsi_g(iq) = V(iq) * Basis%tab_basis(1)%d0gb(iq1,ib1)*Basis%tab_basis(2)%d0gb(iq2,ib2) ! potential part
-           OpPsi_g(iq) = OpPsi_g(iq) -HALF/mass * Basis%tab_basis(1)%d2gb(iq1,ib1,1,1) &
-                                     -HALF/mass * Basis%tab_basis(2)%d2gb(iq2,ib2,1,1)
-              WT(iq)=Basis%tab_basis(1)%w(iq1)*Basis%tab_basis(2)%w(iq2)
+           OpPsi_g(iq) = OpPsi_g(iq) -HALF/mass *(TWO*Basis%tab_basis(1)%d2gb(iq1,ib1,1,1) &
+                                                *Basis%tab_basis(2)%d2gb(iq2,ib2,1,1))
+           OpPsi_g(iq) = OpPsi_g(iq) -HALF/mass *(Basis%tab_basis(1)%d2gb(iq1,ib1,1,1)     &
+                                                *Basis%tab_basis(2)%d0gb(iq2,ib2,1,1))
+           OpPsi_g(iq) = OpPsi_g(iq) -HALF/mass *(Basis%tab_basis(1)%d0gb(iq1,ib1,1,1)     &
+                                                *Basis%tab_basis(2)%d2gb(iq2,ib2,1,1))
 
-           OpPsi_g = OpPsi_g *WT(iq)! Basis%tab_basis(1)%w(iq1)*Basis%tab_basis(2)%w(iq2)
+           WT(iq)=Basis%tab_basis(1)%w(iq1)*Basis%tab_basis(2)%w(iq2)
+           OpPsi_g(iq) = OpPsi_g(iq) *WT(iq)
          END DO
          END DO
     !    OpPsi_g = OpPsi_g -HALF/mass * Basis%tab_basis(1)%d2gb(:,ib,1,1) -HALF/mass * Basis%tab_basis(2)%d2gb(:,ib,1,1)
         ! OpPsi_g is a vector on the grid. It must be projected on the basis (integration)
       !  OpPsi_g = OpPsi_g * Basis%tab_basis(1)%w*Basis%tab_basis(2)%w
          jb=0
-
          DO jb1=1,Basis%tab_basis(1)%nb
          DO jb2=1,Basis%tab_basis(2)%nb
            jb=jb+1
@@ -121,13 +124,11 @@ contains
              Op%RMat(jb,ib) = Op%RMat(jb,ib)+ Basis%tab_basis(1)%d0gb(iq1,jb1)*Basis%tab_basis(2)%d0gb(iq2,jb2)*OpPsi_g(iq)
            END DO
            END DO
-
          END DO
          END DO
        END DO
        END DO
         CALL write_Op(Op)
-            STOP
     ELSE IF( Basis_IS_allocated(Basis)) THEN
         CALL alloc_Op(Op,Basis%nb)
 
@@ -154,14 +155,8 @@ contains
            Op%RMat(jb,ib) = dot_product(Basis%d0gb(:,jb),OpPsi_g)
         END DO
       END DO
-
-
       CALL write_Op(Op)
-
     ELSE
-    !END IF
-
-  !  IF (.NOT. Basis_IS_allocated(Basis)) THEN
       STOP 'ERROR in Set_Op: the Basis is not initialized'
     END IF
 
